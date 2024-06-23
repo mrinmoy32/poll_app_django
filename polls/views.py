520382db907f348bff402187bc2b1e4dca675b3e
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-06-23 22:37:49 +0530

    comment out old views

--- polls/views.py
+++ polls/views.py
@@ -7,27 +7,27 @@ from .models import Question, Choice
 from django.views import generic
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
-    # Below expression also works
-    # return render(request, "polls/index.html", {"latest_question_list": latest_question_list}) 
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     template = loader.get_template("polls/index.html")
+#     context = {
+#         "latest_question_list": latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+#     # Below expression also works
+#     # return render(request, "polls/index.html", {"latest_question_list": latest_question_list}) 
 
-def detail(request, question_id):
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    # return render(request, "polls/detail.html", {"question": question})
+# def detail(request, question_id):
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question does not exist")
+#     # return render(request, "polls/detail.html", {"question": question})
 
-    # Using get_object_or_404
+#     # Using get_object_or_404
 
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {"question": question})
 
 ##### Using generic views from Django
 class IndexView(generic.ListView):
