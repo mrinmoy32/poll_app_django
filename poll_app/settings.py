84575e0edbb291eb5f9f374be3ac0e30e137368b
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-06-26 21:51:18 +0530

    debug_toolbar

--- poll_app/settings.py
+++ poll_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "debug_toolbar", # Add debug_toolbar app
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware", # Add debug_toolbar middleware
 ]
 
 ROOT_URLCONF = 'poll_app.urls'
@@ -122,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# The Debug Toolbar is shown only if your IP address is listed in Django’s INTERNAL_IPS setting. 
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
